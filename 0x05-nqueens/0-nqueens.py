cbfeef823b6ff08444cb864f391c4048a9b54188
Thabiso <940303tabiso@gmail.com>
2023-04-29 11:36:21 +0000

    Task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,45 +1,28 @@
 #!/usr/bin/python3
-"N Queens"
+"""N Queens"""
+
 import sys
 
-def nqueens(n):
-    def is_valid(board, row, col):
-        for r, c in enumerate(board):
-            if c == col or r + c == row + col or r - c == row - col:
-                return False
-        return True
-    
-    def solve(board, row):
-        if row == n:
-            print_board(board)
-            return True
-        for col in range(n):
-            if is_valid(board, row, col):
-                board[row] = col
-                solve(board, row + 1)
-                board[row] = -1
-    
-    def print_board(board):
-        for r in range(n):
-            row = ['.' for _ in range(n)]
-            row[board[r]] = 'Q'
-            print(' '.join(row))
-        print()
-    
-    if not n.isdigit():
-        print('N must be a number')
-        sys.exit(1)
-    
-    n = int(n)
-    if n < 4:
-        print('N must be at least 4')
-        sys.exit(1)
-    
-    board = [-1] * n
-    solve(board, 0)
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: nqueens N')
-        sys.exit(1)
-    nqueens(sys.argv[1])
+def nqueens(n: int):
+    """
+    backtracking
+    """
+    matrix = [[0 for x in range(n)] for y in range(n)]
+    print(str(matrix))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        exit(1)
+
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    nqueens(int(sys.argv[1]))
